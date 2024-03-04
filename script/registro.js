d4e9948218c2d9fa4b0a99667216b54792109240
Carlos Garcia <cargarcia085@gmail.com>
2024-03-03 18:26:38 -0700

    Correcion de codigo del login y de registrar usuarios

--- script/registro.js
+++ script/registro.js
@@ -1,1 +1,4 @@
-
+document.getElementById("btnVolver").addEventListener("click", function() {
+    console.log("Botón de volver clickeado");
+    window.location.href = "index.html";
+});
--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
- // Escucha el evento de clic en el botón de inicio de sesión
- document.getElementById("login-button").addEventListener("click", function(event) {
+// Escucha el evento de clic en el botón de inicio de sesión
+document.getElementById("login-button").addEventListener("click", function(event) {
   // Redirigir a la página de productos sin verificar las credenciales
-  window.location.href = "../inicio.html";
+  window.location.href = "inicio.html";
 });
